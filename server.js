4a20aa674b1ada81fd86d401b1fb74792418d431
Justakidz <justakidz1402@gmail.com>
2023-10-02 00:47:42 +0700

    done fix api

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from "express"
 import * as dotenv from 'dotenv'
 import { userRouter, curriculumRouter } from './routes/index.js'
 import connectDB from "./database/database.js"
+import cors from "cors";
 
 dotenv.config()
 const app = express()
 app.use(express.json()) // Config cho express lam viec voi du lieu theo dinh dang json
+app.use(cors())
 
 // Routes:
 app.use('/users', userRouter)
