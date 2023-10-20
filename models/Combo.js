39af276ef3cb70f32fd1ad9c7efea653c85a65b4
C_CoKi <trandinhcuongfpt2022@gmail.com>
2023-10-20 15:04:06 +0700

    set status and delete po plo

---
+++ models/Combo.js
@@ -0,0 +1,20 @@
+// import mongoose, { Schema, ObjectId } from "mongoose";
+
+// const Combo = mongoose.model("Combo", new Schema({
+//     combo_id: { type: ObjectId },
+//     combo_name: { type: String },
+//     elective_subjectT: { type: String },
+//     note: { type: String },
+//     status: { type: Boolean },
+//     type: { type: String },
+//     curriculum:{
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Curriculum"
+//     },
+//     syllabus:{
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Syllabus"
+//     }
+// }));
+
+// export default Combo;
